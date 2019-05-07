a60e1036604b6c3d9ff85803ea46a8ba5bdaf257
Priyansh <pradadiya667@gmail.com>
2019-05-07 19:56:21 -0400

    added orm.js with the object to create dynamic query

--- config/orm.js
+++ config/orm.js
@@ -1,1 +1,33 @@
 var connection = require("./connection");
+
+var orm = {
+  tableInfo: function(column, table) {
+    connection.query("Select ?? from ??", [column, table], function(
+      err,
+      result
+    ) {
+      if (err) throw err;
+      console.log(result);
+    });
+  },
+  conditional: function(column, table, where) {
+    connection.query(
+      "Select ?? from ?? where ?",
+      [column, table, where],
+      function(err, result) {
+        if (err) throw err;
+        console.log(result);
+      }
+    );
+  },
+  join: function(column, tableOne, tableTwo, tableOneId, tabletwoId) {
+    connection.query(
+      "select?? from ?? as tb1 Inner join ?? as tb2 on tb1.?? = tb2.??",
+      [column, tableOne, tableTwo, tableOneId, tabletwoId],
+      function(err, result) {
+        if (err) throw err;
+        console.log(result);
+      }
+    );
+  }
+};
